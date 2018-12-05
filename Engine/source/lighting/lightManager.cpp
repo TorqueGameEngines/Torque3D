866ff4999383f7da82c82dd20e628cc0df41fe77
Azaezel <quillus@hotmail.com>
2018-12-05 10:55:29 -0600

    retweaked filter for wether ot not to supress the zonelight culling methods when not in the diffuse pass

--- Engine/source/lighting/lightManager.cpp
+++ Engine/source/lighting/lightManager.cpp
@@ -198,7 +198,7 @@ void LightManager::setSpecialLight( LightManager::SpecialLightTypesEnum type, Li
    registerGlobalLight( light, NULL );
 }
 
-void LightManager::registerGlobalLights( const Frustum *frustum, bool staticLighting, bool cullSceneLights)
+void LightManager::registerGlobalLights( const Frustum *frustum, bool staticLighting, bool enableZoneLightCulling)
 {
    PROFILE_SCOPE( LightManager_RegisterGlobalLights );
 
@@ -226,7 +226,7 @@ void LightManager::registerGlobalLights( const Frustum *frustum, bool staticLigh
    {
       // Cull the lights using the frustum.
       getSceneManager()->getContainer()->findObjectList( *frustum, lightMask, &activeLights );
-	  if (cullSceneLights)
+	  if (enableZoneLightCulling)
 	  {
 		  for (U32 i = 0; i < activeLights.size(); ++i)
 		  {

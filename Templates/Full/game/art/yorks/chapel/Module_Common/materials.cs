c6e4b4a0e4a111135d73a3aef9e4f73322774953
Areloch <areloch@gmail.com>
2019-03-04 17:57:53 -0600

    Corrected yorks chapel materials + added to the main test level

--- Templates/Full/game/art/yorks/chapel/Module_Common/materials.cs
+++ Templates/Full/game/art/yorks/chapel/Module_Common/materials.cs
@@ -48,25 +48,29 @@ singleton Material(LMA_chapel_int_chapel_Module_Common_LMA_chapel_int_Material1)
    specularMap[0] = "art/shapes/yorks/chapel/brick_small_blue_dds.dds";
 };
 
-singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Material0)
+singleton Material(chapel_Module_Common_LMX_chapel_props_Material0)
 {
    mapTo = "chapel_Module_Common_LMX_chapel_props-Material0";
    diffuseColor[1] = "1 1 1 0.8";
-   diffuseMap[0] = "art/shapes/yorks/chapel/marble1_red.png";
-   diffuseMap[1] = "art/shapes/yorks/chapel/marble1_red.png";
-   toneMap[0] = "art/shapes/yorks/chapel/Module_Common/LMX_chapel_props.dds";
+   diffuseMap[0] = "art/yorks/chapel/marble1_red.png";
+   diffuseMap[1] = "marble1_red.png";
+   toneMap[0] = "art/yorks/chapel/Module_Common/LMX_chapel_props.png";
    emissive[1] = "1";
    translucentBlendOp = "None";
    emissive[0] = "0";
    materialTag0 = "Miscellaneous";
    alphaRef = "0";
-   lightMap[1] = "art/shapes/yorks/chapel/Module_Common/LMX_chapel_props.dds";
+   lightMap[1] = "art/yorks/chapel/Module_Common/LMX_chapel_props.dds";
    castShadows = "0";
    cubemap = "DesertSkyCubemap";
    showFootprints = "0";
+   smoothness[0] = "1";
+   specular0 = "1 1 1 1";
+   specularPower0 = "8";
+   pixelSpecular0 = "0";
 };
 
-singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Material1)
+singleton Material(chapel_Module_Common_LMX_chapel_props_Material1)
 {
 	diffuseColor[0] = "1 1 1 1";
 	specular[0] = "1 1 1 1";
@@ -77,17 +81,21 @@ singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Materi
 	translucent = false;
 	translucentBlendOp = "None";
 	mapTo = "chapel_Module_Common_LMX_chapel_props-Material1";
-	toneMap[0] = "art/shapes/yorks/chapel/Module_Common/LMX_chapel_props.dds";
-	diffuseMap[0] = "art/shapes/yorks/chapel/marble1_green.png";
+	toneMap[0] = "art/yorks/chapel/Module_Common/LMX_chapel_props.png";
+	diffuseMap[0] = "art/yorks/chapel/marble1_green.png";
    diffuseColor[1] = "1 1 1 0.7";
-   diffuseMap[1] = "art/shapes/yorks/chapel/marble1_green.png";
-   lightMap[1] = "art/shapes/yorks/chapel/Module_Common/LMX_chapel_props.dds";
+   diffuseMap[1] = "marble1_green.png";
+   lightMap[1] = "art/yorks/chapel/Module_Common/LMX_chapel_props.dds";
    emissive[1] = "1";
    materialTag0 = "Miscellaneous";
    cubemap = "DesertSkyCubemap";
+   smoothness[0] = "1";
+   specular0 = "1 1 1 1";
+   specularPower0 = "8";
+   pixelSpecular0 = "0";
 };
 
-singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Material2)
+singleton Material(chapel_Module_Common_LMX_chapel_props_Material2)
 {
 	diffuseColor[0] = "1 1 1 1";
 	specular[0] = "1 1 1 1";
@@ -98,13 +106,18 @@ singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Materi
 	translucent = false;
 	translucentBlendOp = "None";
 	mapTo = "chapel_Module_Common_LMX_chapel_props-Material2";
-	toneMap[0] = "art/shapes/yorks/chapel/Module_Common/LMX_chapel_props.dds";
-	diffuseMap[0] = "art/shapes/yorks/chapel/int_props_map.png";
+	toneMap[0] = "art/yorks/chapel/Module_Common/LMX_chapel_props.png";
+	diffuseMap[0] = "art/yorks/chapel/int_props_map.png";
    diffuseColor[1] = "1 1 1 0.7";
-   diffuseMap[1] = "art/shapes/yorks/chapel/int_props_map.png";
-   lightMap[1] = "art/shapes/yorks/chapel/Module_Common/LMX_chapel_props.dds";
+   diffuseMap[1] = "int_props_map.png";
+   lightMap[1] = "LMX_chapel_props.dds";
    emissive[1] = "1";
    cubemap = "DesertSkyCubemap";
+   smoothness[0] = "1";
+   specular0 = "1 1 1 1";
+   materialTag0 = "Miscellaneous";
+   specularPower0 = "8";
+   pixelSpecular0 = "1";
 };
 
 singleton Material(SB_chapel_glass_chapel_Module_Common_SB_chapel_glass_Material0)
@@ -128,4 +141,66 @@ singleton Material(BlankWhite)
    mapTo = "chapel_Module_Common_SB_chapel_ext-Material0";
    emissive[0] = "0";
    materialTag0 = "Miscellaneous";
+};
+
+singleton Material(SB_chapel_ext_chapel_Module_Common_SB_chapel_ext_Material0)
+{
+   mapTo = "chapel_Module_Common_SB_chapel_ext-Material0";
+   smoothness[0] = "1";
+   translucentBlendOp = "None";
+};
+
+singleton Material(LMA_chapel_int_chapel_Module_Common_LMA_chapel_int_Material1)
+{
+   mapTo = "chapel_Module_Common_LMA_chapel_int-Material1";
+   diffuseMap[0] = "art/yorks/chapel/brick_small_blue.png";
+   smoothness[0] = "1";
+   translucentBlendOp = "None";
+   toneMap[0] = "art/yorks/chapel/Module_Common/LMA_chapel_int.png";
+};
+
+singleton Material(LMA_chapel_int_chapel_Module_Common_LMA_chapel_int_Material0)
+{
+   mapTo = "chapel_Module_Common_LMA_chapel_int-Material0";
+   diffuseMap[0] = "art/yorks/chapel/brick_block.png";
+   smoothness[0] = "1";
+   translucentBlendOp = "None";
+   toneMap[0] = "art/yorks/chapel/Module_Common/LMA_chapel_int.png";
+};
+
+singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Material1)
+{
+   mapTo = "chapel_Module_Common_LMX_chapel_props-Material1";
+   diffuseMap[0] = "art/yorks/chapel/marble1_green.png";
+   smoothness[0] = "1";
+   translucentBlendOp = "None";
+   toneMap[0] = "art/yorks/chapel/Module_Common/LMX_chapel_props.png";
+};
+
+singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Material2)
+{
+   mapTo = "chapel_Module_Common_LMX_chapel_props-Material2";
+   diffuseMap[0] = "art/yorks/chapel/int_props_map.png";
+   smoothness[0] = "1";
+   translucentBlendOp = "None";
+   toneMap[0] = "art/yorks/chapel/Module_Common/LMX_chapel_props.png";
+};
+
+singleton Material(LMX_chapel_props_chapel_Module_Common_LMX_chapel_props_Material0)
+{
+   mapTo = "chapel_Module_Common_LMX_chapel_props-Material0";
+   diffuseMap[0] = "art/yorks/chapel/marble1_red.png";
+   smoothness[0] = "1";
+   translucentBlendOp = "None";
+   toneMap[0] = "art/yorks/chapel/Module_Common/LMX_chapel_props.png";
+};
+
+singleton Material(SB_chapel_glass_chapel_Module_Common_SB_chapel_glass_Material0)
+{
+   mapTo = "chapel_Module_Common_SB_chapel_glass-Material0";
+   smoothness[0] = "1";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/yorks/chapel/stainedglass1.png";
+   glow[0] = "1";
+   emissive[0] = "1";
 };

de8b267780a4d16d5bc9bf08e421b1fefe7eaf96
Azaezel <quillus@hotmail.com>
2019-03-24 17:41:34 -0500

    was improperly accounting for attenaution. needs a reversal for atten = 0 to be full unblended application fo a given projection.

--- Templates/Full/game/shaders/common/lighting/advanced/reflectionProbeArrayP.hlsl
+++ Templates/Full/game/shaders/common/lighting/advanced/reflectionProbeArrayP.hlsl
@@ -71,7 +71,7 @@ float getDistBoxToPoint(float3 pt, float3 extents)
 float defineBoxSpaceInfluence(Surface surface, ProbeData probe, float3 wsEyeRay)
 {
    float3 surfPosLS = mul(probe.worldToLocal, float4(surface.P, 1.0)).xyz;
-   float atten = probe.attenuation;
+   float atten = 1.0-probe.attenuation;
    float baseVal = 0.25;
    float dist = getDistBoxToPoint(surfPosLS,float3(baseVal,baseVal,baseVal));
    return saturate(smoothstep(baseVal+0.0001,atten*baseVal,dist));

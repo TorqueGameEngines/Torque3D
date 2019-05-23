5ca2a5eb5167346cde9e23cba6c4085fc4092665
Brian Roberts <quillus@hotmail.com>
2019-05-23 02:27:02 -0500

    shift openal-soft directory add plus various cmake set(values) on over to an o/s agnostic block, rather than only including that if used on windows

--- Tools/CMake/torque3d.cmake
+++ Tools/CMake/torque3d.cmake
@@ -61,8 +61,9 @@ mark_as_advanced(TORQUE_BASIC_LIGHTING)
 option(TORQUE_SFX_DirectX "DirectX Sound" OFF)
 mark_as_advanced(TORQUE_SFX_DirectX)
 option(TORQUE_SFX_OPENAL "OpenAL Sound" ON)
-#windows uses openal-soft
-if(WIN32)
+
+if(TORQUE_SFX_OPENAL)
+
     #disable a few things that are not required
     set(ALSOFT_TESTS OFF CACHE BOOL "Build and install test programs" FORCE)
     set(ALSOFT_UTILS OFF CACHE BOOL "Build and install utility programs" FORCE)
@@ -72,15 +73,14 @@ if(WIN32)
     set(ALSOFT_NO_CONFIG_UTIL OFF CACHE BOOL "Disable building the alsoft-config utility" FORCE)
     set(ALSOFT_HRTF_DEFS OFF CACHE BOOL "Install HRTF definition files" FORCE)
     set(ALSOFT_AMBDEC_PRESETS OFF CACHE BOOL "Install AmbDec presets" FORCE)
-    
-    add_subdirectory( ${libDir}/openal-soft ${CMAKE_CURRENT_BINARY_DIR}/openal-soft)
-endif()
 
-if(TORQUE_SFX_OPENAL)
+    add_subdirectory( ${libDir}/openal-soft ${CMAKE_CURRENT_BINARY_DIR}/openal-soft)
+
     #Hide some unnecessary fields as advanced
     mark_as_advanced(ALSOFT_AMBDEC_PRESETS)
     mark_as_advanced(ALSOFT_BACKEND_DSOUND)
     mark_as_advanced(ALSOFT_BACKEND_MMDEVAPI)
+    mark_as_advanced(ALSOFT_BUILD_ROUTER)
     mark_as_advanced(ALSOFT_BACKEND_WAVE)
     mark_as_advanced(ALSOFT_BACKEND_WINMM)
     mark_as_advanced(ALSOFT_CONFIG)

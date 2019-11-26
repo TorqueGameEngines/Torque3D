337603956614e3109179346da805d347fb13a581
rextimmy <rextimm777@gmail.com>
2019-11-26 22:09:05 +1000

    Old template is dead

--- Templates/Full/game/art/yorks/chapel/Module_Common/LMA_chapel_int.cs
+++
@@ -1,7 +0,0 @@
-
-singleton TSShapeConstructor(LMA_chapel_intDae)
-{
-   baseShape = "./LMA_chapel_int.dae";
-   unit = "1";
-   loadLights = "0";
-};
--- Templates/Full/game/art/yorks/models/LMA_area5.cs
+++
@@ -1,7 +0,0 @@
-
-singleton TSShapeConstructor(LMA_area5Dae)
-{
-   baseShape = "./LMA_area5.dae";
-   unit = "0.01";
-   loadLights = "0";
-};

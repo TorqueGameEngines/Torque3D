d3f51cf7c095740af3dd6ff3eeec97c363625311
Pecon <admin@pecon.us>
2020-08-11 12:51:46 -0700

    Basic Platform::openWebBrowser implementation for linux

--- Engine/source/platformX86UNIX/x86UNIXPlatform.cpp
+++ Engine/source/platformX86UNIX/x86UNIXPlatform.cpp
@@ -1,8 +1,26 @@
 #include "platform/platform.h"
+#include <string>
 
 bool Platform::openWebBrowser( const char* webAddress )
 {
-    return false; // TODO LINUX
+    std::string startingURL(webAddress);
+    std::string filteredURL;
+
+    unsigned short length = startingURL.length();
+    for(unsigned short i = 0; i < length; i++)
+    {
+        filteredURL = filteredURL + '\\' + startingURL.at(i);
+    }
+
+    std::string runCommand = "URL=" + filteredURL + "; xdg-open $URL > /dev/null 2> /dev/null";
+
+    short statusCode;
+    statusCode = system(runCommand.c_str());
+
+    if(statusCode == 0)
+        return true;
+
+    return false;
 }
 
 #ifdef TORQUE_DEDICATED

ed260548c57bbe8710c1c63035b66463d81f3620
AzaezelX <quillus@hotmail.com>
2020-10-08 19:14:50 -0500

    fix for empty r channel creation of composites crashing out

--- Engine/source/gfx/gfxTextureManager.cpp
+++ Engine/source/gfx/gfxTextureManager.cpp
@@ -1196,7 +1196,10 @@ GFXTextureObject *GFXTextureManager::createCompositeTexture(GBitmap*bmp[4], U32 
    {
       for (U32 y = 0; y < bmp[lastValidTex]->getHeight(); y++)
       {
-         rChan = bmp[0]->getChanelValueAt(x, y, inputKey[0]);
+         if (bmp[0])
+            rChan = bmp[0]->getChanelValueAt(x, y, inputKey[0]);
+         else
+            gChan = 255;
 
          if (bmp[1])
             gChan = bmp[1]->getChanelValueAt(x, y, inputKey[1]);

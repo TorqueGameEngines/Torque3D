c0203e13e93b49c8c6d60996b97c1db014dad1a6
Areloch <Areloch@gmail.com>
2020-10-16 22:12:05 -0500

    Basic prototyping module initial upload

---
+++ Templates/BaseGame/game/data/Prototyping/Materials/InteractiveRed.cs
@@ -0,0 +1,6 @@
+//--- OBJECT WRITE BEGIN ---
+singleton Material(InteractiveRed) {
+   mapTo="InteractiveRed";
+   DiffuseMapAsset = "Prototyping:InteractiveRed_ALBEDO";
+};
+//--- OBJECT WRITE END ---
---
+++ Templates/BaseGame/game/data/Prototyping/Materials/WaterBlue.cs
@@ -0,0 +1,6 @@
+//--- OBJECT WRITE BEGIN ---
+singleton Material(WaterBlue) {
+   mapTo="WaterBlue";
+   DiffuseMapAsset = "Prototyping:WaterBlue_ALBEDO";
+};
+//--- OBJECT WRITE END ---

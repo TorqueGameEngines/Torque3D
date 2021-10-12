7a78ec46c8e88dd8653ad6db3999be1e59ddd59c
Robert MacGregor <ragoradev@gmail.com>
2021-10-12 17:20:23 -0400

    * [lpng] BugFix: Correct some compiler warnings associated with lpng and SSE optimizations. This also enables SSE when available for lpng.

--- Tools/CMake/libraries/lpng.cmake
+++ Tools/CMake/libraries/lpng.cmake
@@ -29,6 +29,11 @@ project(lpng)
 set(PNG_ARM_NEON off CACHE STRING "")
 add_definitions(-DPNG_ARM_NEON_OPT=0)
 
+# Enables SSE for libpng - also takes care of compiler warnings.
+# If we don't want SSE, we should set it to off/0.
+set(PNG_INTEL_SSE on CACHE STRING "")
+add_definitions(-DPNG_INTEL_SSE_OPT=1)
+
 addInclude(${libDir}/zlib)
 
 finishLibrary("${libDir}/${PROJECT_NAME}")

4f4184ab683e80192cc00cc5c358abca0bca5cec
Johan Mattsson <39247600+mjunix@users.noreply.github.com>
2023-03-04 21:44:52 +0100

    Assign result from `mClampF`

--- Engine/source/lighting/common/projectedShadow.cpp
+++ Engine/source/lighting/common/projectedShadow.cpp
@@ -395,7 +395,7 @@ void ProjectedShadow::_calcScore( const SceneRenderState *state )
    F32 secs = mFloor( (F32)msSinceLastRender / 1000.0f );
    
    mScore = pct + secs;
-   mClampF( mScore, 0.0f, 2000.0f );
+   mScore = mClampF( mScore, 0.0f, 2000.0f );
 }
 
 void ProjectedShadow::update( const SceneRenderState *state )

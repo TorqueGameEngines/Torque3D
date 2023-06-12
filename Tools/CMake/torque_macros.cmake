fa14b8c3a910637b95e46576d0f6e6b5bc747194
AzaezelX <quillus@hotmail.com>
2023-06-12 09:01:50 -0500

    framework macro typofix

--- Tools/CMake/torque_macros.cmake
+++ Tools/CMake/torque_macros.cmake
@@ -128,6 +128,6 @@ endmacro (filterOut)
 ################# apple frameworks ###################
 macro(addFramework framework)
 	if (APPLE)
-		set(TORQUE_LINK_LIBRARIES ${TORQUE_LINK_LIBRARIES}"-framework ${framework}")
+		set(TORQUE_LINK_LIBRARIES ${TORQUE_LINK_LIBRARIES} "-framework ${framework}")
 	endif()
 endmacro()

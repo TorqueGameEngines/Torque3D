1762aaf7966797e3db395853fd02bf59ab854b62
marauder2k7 <ctrl-intelligence@live.co.uk>
2023-07-21 20:34:20 +0100

    mac

--- Tools/CMake/torque3d.cmake
+++ Tools/CMake/torque3d.cmake
@@ -22,10 +22,6 @@
 
 project(${TORQUE_APP_NAME})
 
-if ( CMAKE_COMPILER_IS_GNUCC )
-    set(CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -w")
-endif()
-
 if(UNIX)
     if(NOT CXX_FLAG32)
         set(CXX_FLAG32 "")

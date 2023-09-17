a36a35c39ef4b83f0a8bdeca2cb2608933ccc1b8
AzaezelX <quillus@hotmail.com>
2023-09-17 17:15:43 -0500

    clean up folder display of optional core modules

--- Tools/CMake/modules/navigation.cmake
+++ Tools/CMake/modules/navigation.cmake
@@ -3,11 +3,13 @@ option(TORQUE_NAVIGATION "Enable Navigation module" ON)
 
 if(TORQUE_NAVIGATION)
   message("Enabling Navigation Module")
-
-  torqueAddSourceDirectories("${CMAKE_SOURCE_DIR}/Engine/source/navigation")
+  
+  file(GLOB_RECURSE TORQUE_NAV_SOURCES "${CMAKE_SOURCE_DIR}/Engine/source/navigation/*.cpp" "${CMAKE_SOURCE_DIR}/Engine/source/navigation/*.h" )
+  set(TORQUE_SOURCE_FILES ${TORQUE_SOURCE_FILES} ${TORQUE_NAV_SOURCES})
   set(TORQUE_LINK_LIBRARIES ${TORQUE_LINK_LIBRARIES} recast)
   set(TORQUE_COMPILE_DEFINITIONS ${TORQUE_COMPILE_DEFINITIONS} recast TORQUE_NAVIGATION_ENABLED)
 
   # Since recast lives elsewhere we need to ensure it is known to Torque when providing a link to it
   add_subdirectory("${CMAKE_SOURCE_DIR}/Engine/lib/recast" ${CMAKE_BINARY_DIR}/temp/recast EXCLUDE_FROM_ALL)
+  source_group(TREE "${CMAKE_SOURCE_DIR}/Engine/source/navigation/" PREFIX "Modules/NAVIGATION" FILES ${TORQUE_NAV_SOURCES})
 endif(TORQUE_NAVIGATION)
--- Tools/CMake/modules/tools.cmake
+++ Tools/CMake/modules/tools.cmake
@@ -4,8 +4,8 @@ option(TORQUE_TOOLS "Enable Torque Tools" ON)
 if(TORQUE_TOOLS)
   message("Enabling Torque Tools Module")
 
-  file(GLOB TORQUE_TOOLS_SOURCES "gui/editor/*.cpp" "gui/worldEditor/*.cpp" "gui/worldEditor/tools/*.cpp"
-                                 "gui/editor/inspector/*.cpp")
+  file(GLOB_RECURSE TORQUE_TOOLS_SOURCES "gui/editor/*.cpp" "gui/worldEditor/*.cpp")
   set(TORQUE_SOURCE_FILES ${TORQUE_SOURCE_FILES} ${TORQUE_TOOLS_SOURCES})
   set(TORQUE_COMPILE_DEFINITIONS ${TORQUE_COMPILE_DEFINITIONS} TORQUE_TOOLS)
+  source_group(TREE "${CMAKE_SOURCE_DIR}/Engine/source/gui/" PREFIX "Modules/TOOLS" FILES ${TORQUE_TOOLS_SOURCES})
 endif(TORQUE_TOOLS)

8fc56dfd9c6ba6e2fceca157cb11e95bacc473d6
marauder2k7 <ctrl-intelligence@live.co.uk>
2024-03-25 00:17:54 +0000

    remove speex cpack
    cpack was causing issues with compiling remove any references to it.
    
    change all options being set for the new libs to use the advanced_option macro
    
    remove speex, opus is better.

--- Tools/CMake/torque_configs.cmake
+++ Tools/CMake/torque_configs.cmake
@@ -15,7 +15,7 @@ set(TORQUE_COMPILE_DEFINITIONS ICE_NO_DLL PCRE_STATIC TORQUE_ADVANCED_LIGHTING T
 
 # All link libraries. Modules should append to this the path to specify additional link libraries (.a, .lib, .dylib, .so)
 set(TORQUE_LINK_LIBRARIES tinyxml collada squish opcode assimp FLAC FLAC++ ogg vorbis  
-			vorbisfile vorbisenc opus speex sndfile SDL2 glad pcre convexDecomp zlib)
+			vorbisfile vorbisenc opus sndfile SDL2 glad pcre convexDecomp zlib)
 
 if(TORQUE_TESTING)
 set(TORQUE_LINK_LIBRARIES ${TORQUE_LINK_LIBRARIES} gtest gmock)

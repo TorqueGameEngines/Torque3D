d81819a47f629dc2b0e1d6b43d7114049c630bc1
marauder2k7 <ctrl-intelligence@live.co.uk>
2025-01-30 20:34:05 +0000

    more groundwork
    drivers now saved to a list
    sfxInit class
    
    start of mirroring gfx layer.

--- Engine/source/sfx/openal/sfxALVoice.h
+++ Engine/source/sfx/openal/sfxALVoice.h
@@ -26,7 +26,7 @@
 #ifndef _SFXVOICE_H_
    #include "sfx/sfxVoice.h"
 #endif
-#ifndef _openAlInterface
+#ifndef _LOADOAL_H_
    #include "sfx/openal/LoadOAL.h"
 #endif
 #ifndef _PLATFORM_THREADS_MUTEX_H_

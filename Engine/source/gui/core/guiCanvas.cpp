6f9c4158d86a44ba185037ae2aa073cc291ec84f
marauder2k7 <ctrl-intelligence@live.co.uk>
2025-06-18 17:05:35 +0100

    gui offscreen material test
    added interaction static guioffscreencanvas to capture mouse events
    added test shape

--- Engine/source/gui/core/guiCanvas.cpp
+++ Engine/source/gui/core/guiCanvas.cpp
@@ -690,6 +690,12 @@ bool GuiCanvas::processInputEvent(InputEventInfo &inputEvent)
    mConsumeLastInputEvent = true;
    mLastInputDeviceType = inputEvent.deviceType;
 
+   // If we have an active offscreen canvas, give it the input
+   if (GuiOffscreenCanvas::sActiveOffscreenCanvas &&
+      (GuiOffscreenCanvas::sActiveOffscreenCanvas != this) &&
+      GuiOffscreenCanvas::sActiveOffscreenCanvas->processInputEvent(inputEvent))
+      return mConsumeLastInputEvent;
+
    // First call the general input handler (on the extremely off-chance that it will be handled):
    if (mFirstResponder &&  mFirstResponder->onInputEvent(inputEvent))
    {
